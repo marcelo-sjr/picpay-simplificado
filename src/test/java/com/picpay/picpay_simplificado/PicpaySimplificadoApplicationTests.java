1c049f9a841cf1f61f7d9fe71bd79ae729ed63c8
marcelo-sjr <marcelodasilvajr2@gmail.com>
2026-01-27 05:59:14 -0300

    project configuration

---
+++ src/test/java/com/picpay/picpay_simplificado/PicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.picpay_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
