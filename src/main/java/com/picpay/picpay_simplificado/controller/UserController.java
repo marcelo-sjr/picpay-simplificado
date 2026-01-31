d01acb9066df38cbdbd26920d55eb8915e04f925
marcelo-sjr <marcelodasilvajr2@gmail.com>
2026-01-31 10:11:25 -0300

    feat:UserController created

---
+++ src/main/java/com/picpay/picpay_simplificado/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.picpay.picpay_simplificado.controller;
+
+import com.picpay.picpay_simplificado.dto.UserDto;
+import com.picpay.picpay_simplificado.dto.UserRequest;
+import com.picpay.picpay_simplificado.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<UserDto> save(@RequestBody @Valid UserRequest request) {
+        return ResponseEntity.ok(service.save(request));
+    }
+}
