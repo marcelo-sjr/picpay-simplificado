8747541431e90c96508c514d28a5eff279df2db7
marcelo-sjr <marcelodasilvajr2@gmail.com>
2026-02-03 13:01:08 -0300

    refactor: decoupling transactiondto from userdto

--- src/main/java/com/picpay/picpay_simplificado/dto/TransactionDto.java
+++ src/main/java/com/picpay/picpay_simplificado/dto/TransactionDto.java
@@ -1,6 +1,8 @@
 package com.picpay.picpay_simplificado.dto;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
-public record TransactionDto(BigDecimal value, UserDto payer, UserDto payee) {
+public record TransactionDto(UUID public_id, LocalDateTime timeStamp, BigDecimal value, Long payer_id, Long payee_id) {
 }
